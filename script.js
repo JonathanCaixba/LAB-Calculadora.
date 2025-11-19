fbb659a824343b77bd0af4f1cba52116b133b8b2
Jonathan Caixba <jonathan.caixba99@unach.mx>
2025-11-19 13:55:23 -0600

    feat: se introduce codigo al script

---
+++ script.js
@@ -0,0 +1,38 @@
+// <-- Inicio -->
+
+// Funciones
+function division (a, b){
+    let total = a / b;
+    return total;
+}
+
+function multiplicacion (a, b){
+    let total = a * b;
+    return total;
+}
+
+function sumar (a, b){
+    let total = a + b;
+    return total;
+}
+
+function resta (a, b){
+    let total = a - b;
+    return total;
+}
+
+// Proceso
+let resultadoDivi = division(10, 5);
+console.log("Esta es la division:", resultadoDivi);
+
+let resultadoMulti = multiplicacion(10, 5);
+console.log("Esta es la multiplicacion:", resultadoMulti);
+
+let resultadoSuma = sumar(10, 5);
+console.log("Esta es la suma:", resultadoSuma);
+
+let resultadoRest = resta(10, 5);
+console.log("Esta es la resta:", resultadoRest);
+
+// <-- FIN -->
+// <-- By: Jonathan Caixba -->
